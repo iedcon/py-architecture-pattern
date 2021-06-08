a81ef16eb418abf35bd473528d7afc7c205826ef
iedcon <iedcon12@gmail.com>
2021-06-08 21:17:52 +0900

    chapter04-service layer

--- src/model.py
+++ src/model.py
@@ -18,6 +18,21 @@ class Batch:
         self._purchased_quantity = qty
         self._allocations = set()
 
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
             self._allocations.add(line)
@@ -36,21 +51,6 @@ class Batch:
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
-
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, Batch):
-            return False
-        return other.reference == self.reference
-
-    def __hash__(self) -> int:
-        return hash(self.reference)
-
-    def __gt__(self, other):
-        if self.eta is None:
-            return False
-        if other.eta is None:
-            return True
-        return self.eta > other.eta
 
 
 class OutOfStock(Exception):
---
+++ src/services.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+from datetime import date
+from . import model, repository
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku: str, batches: List[model.Batch]) -> bool:
+    print(batches)
+    return sku in {b.sku for b in batches}
+
+
+def add_batch(
+    ref: str, sku: str, qty: int, eta: Optional[date],
+    repo: repository.AbstractRepository, session,
+) -> None:
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
+
+
+def allocate(line: model.OrderLine, repo: repository.AbstractRepository, session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f'Invalid sku {line.sku}')
+    batchref = model.allocate(line, batches)
+    session.commit()
+
+    return batchref
+
+
+def deallocate(line: model.OrderLine, batch: model.Batch, session):
+    batch.deallocate(line)

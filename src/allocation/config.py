e0c6659769ad80e0a1d049a3276cdc3c4943d583
iedcon <iedcon12@gmail.com>
2021-06-11 00:09:51 +0900

    alembic migration

--- src/allocation/config.py
+++ src/allocation/config.py
@@ -3,9 +3,9 @@ import os
 
 def get_postgres_uri():
     host = os.environ.get("DB_HOST", "localhost")
-    port = 54321 if host == "localhost" else 5432
-    password = os.environ.get("DB_PASSWORD", "abc123")
-    user = os.environ.get("DB_USER", "allocation")
+    port = 5432 if host == "localhost" else 5432
+    password = os.environ.get("DB_PASSWORD", "1234")
+    user = os.environ.get("DB_USER", "leo")
     db_name = "allocation"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
